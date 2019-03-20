8c363024fc84a593b334ea6c0ed8ab29a0f63dbf
Grzegorz-Pietruszka <42655303+Grzegorz-Pietruszka@users.noreply.github.com>
2019-03-20 15:39:40 +0000

    Update weather.js

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ class Weather {
 
 //Fetch wheater from the API
 async getWeather() {
-    const response = await fetch(`http://api.apixu.com/v1/current.json?key=${this.apiKey}&q=${this.city}`);
+    const response = await fetch(`https://api.apixu.com/v1/current.json?key=${this.apiKey}&q=${this.city}`);
     const resposneData = await response.json();
 
     return resposneData;
